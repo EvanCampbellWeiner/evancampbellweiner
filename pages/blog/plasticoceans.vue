6dfcadc02043338146e9ee6f89c677ff74a5d515
Evan Campbell-Weiner <evancampbellweiner@gmail.com>
2022-01-19 11:47:57 -0500

    Added plastic oceans blog post. Fixed layout issues with experience page.

--- pages/blog/plasticoceans.vue
+++ pages/blog/plasticoceans.vue
@@ -12,14 +12,14 @@
 export default {
    data:  function() {
       return {
-         title: 'Sequence 21',
+         title: 'Plastic Oceans Calculator',
          paragraph1: 
-         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan nisi non lobortis ultrices. Nam massa massa, ultrices ut sagittis vitae, convallis sit amet lacus. Suspendisse molestie facilisis massa vel tempor. Fusce sit amet suscipit mi. Mauris eget pellentesque velit, vitae consectetur nisi. Duis diam risus, gravida sed diam et, vestibulum tempor nisl. Donec eleifend elit ac tortor vehicula, et blandit metus eleifend. Nam sit amet augue vitae purus luctus efficitur. Fusce quis pulvinar lacus. Mauris facilisis odio quis quam pulvinar various",
+         "The Plastic Oceans Calculator was an HTML/CSS/JS program created for Plastic Oceans Canada built to calculate a persons environmental impact based on their weekly habits. The project was originally created at Capital One's Digital for Good Hackathon where I was recruited to provide front-end support for the project. The team ended up consisting of two other project managers, a backend developer, and a designer. ",
          paragraph2: 
-         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan nisi non lobortis ultrices. Nam massa massa, ultrices ut sagittis vitae, convallis sit amet lacus. Suspendisse molestie facilisis massa vel tempor. Fusce sit amet suscipit mi. Mauris eget pellentesque velit, vitae consectetur nisi. Duis diam risus, gravida sed diam et, vestibulum tempor nisl. Donec eleifend elit ac tortor vehicula, et blandit metus eleifend. Nam sit amet augue vitae purus luctus efficitur. Fusce quis pulvinar lacus. Mauris facilisis odio quis quam pulvinar various",
+         "My portion of the project was mostly just implementing the designs and using jQuery to interact with the python logic created by the backend developer. This included creating a dynamic survey form with the ability to export results based on the users information. Unfortunately, the final integration into wordpress ended up being a massive battle due to the disassociation with the backend.",
          paragraph3: 
-         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan nisi non lobortis ultrices. Nam massa massa, ultrices ut sagittis vitae, convallis sit amet lacus. Suspendisse molestie facilisis massa vel tempor. Fusce sit amet suscipit mi. Mauris eget pellentesque velit, vitae consectetur nisi. Duis diam risus, gravida sed diam et, vestibulum tempor nisl. Donec eleifend elit ac tortor vehicula, et blandit metus eleifend. Nam sit amet augue vitae purus luctus efficitur. Fusce quis pulvinar lacus. Mauris facilisis odio quis quam pulvinar various",
-         img: '/Sequence21.jpg'
+         "Looking back on this project, I really believe I learned a lot about project management. Before the project I was fairly inexperienced and did not have a lot of trust in my skills. But after being the sole frontend developer, I learned the importance of taking ownership of your skills. If I did this project again I would have taken more of a leadership role so that the outcome was a success.",
+         img: '/plasticoceans.jpg',
       }
    }
 }
--- pages/experience.vue
+++ pages/experience.vue
@@ -9,7 +9,7 @@
         <h2 class="text-3xl">Summary of Qualifications</h2>
         <ul
           class="text-xl p-4 mt-4 flex flex-col items-center justify-center border-2 border-white border-solid text-gray-300">
-          <li>2.5 years of Web Development Experience</li>
+          <li>3 Years of Web Application Development using HTML / CSS</li>
           <li>Experience working on over 20 Web Applications</li>
           <li>1+ year experience as a junior developer at iApotheca Healthcare</li>
           <li>1+ year experience freelancing for Overscore Media</li>
@@ -18,7 +18,7 @@
       </div>
       <div class="flex flex-col justify-center items-center border-1 border-white m-6 p-2 text-xl space-y-4">
         <h2 class="text-3xl text-center">Work Experience</h2>
-        <div class="flex flex-col justify-around p-8">
+        <div class="flex flex-col justify-around md:w-1/3">
           <div class="flex flex-row justify-between space-x-16 items-end">
             <h3 class="text-2xl">Junior Developer</h3>
             <p class="text-right text-md text-gray-300">May 2021 - Present <br />
@@ -27,14 +27,13 @@
           <p class="text-md text-gray-300">iApotheca Healthcare</p>
 
           <ul class="text-md  pt-2 list-disc flex flex-col text-gray-300">
-            <li>Experience using Laravel / Livewire to update existing applications.</li>
+            <li>Experience using Laravel / Livewire (TALL Stack) to update existing applications.</li>
             <li>Used Github PR's for feature focused development</li>
-            <li>PHPUnit for Unit and Feature testing</li>
-            <li>Created an API using Postman</li>
-            <li>Used Code with Me / Pair as needed</li>
+            <li>Created feature, unit, and integration tests for new / existing features</li>
+            <li>Provided code reviews to help ensure multi-platform delivery and cross-browser support</li>
           </ul>
         </div>
-        <div class="flex flex-col justify-around">
+        <div class="flex flex-col justify-around md:w-1/3">
           <div class="flex flex-row justify-between space-x-8 items-end">
             <h3 class="text-2xl">Backend Developer</h3>
             <p class="text-right text-md text-gray-300">May 2021 - Present </p>
@@ -42,14 +41,13 @@
           <p class="text-md text-gray-300">Overscore Media</p>
 
           <ul class="text-md  pt-2 list-disc flex flex-col text-gray-300">
-            <li>Experience using Laravel / Livewire to update existing applications.</li>
-            <li>Used Github PR's for feature focused development</li>
-            <li>PHPUnit for Unit and Feature testing</li>
-            <li>Created an API using Postman</li>
-            <li>Used Code with Me / Pair as needed</li>
+            <li>Worked in a team of 3 using Laravel / Inertia JS to create an online store for Stluet Furniture</li>
+            <li>Worked in a team using the TALL stack to create Sequence21</li>
+            <li>Acted as a backend developer to help create database, program structure, stripe integration, and more</li>
+            <li>Provided excellent presentation skills when working with the client and team members</li> 
           </ul>
         </div>
-        <div class="flex flex-col justify-around">
+        <div class="flex flex-col justify-around md:w-1/3">
           <div class="flex flex-row justify-between space-x-8 items-end">
             <h3 class="text-2xl">IT Systems Analyst</h3>
             <p class="text-right text-md text-gray-300">January 2021 - May 2021 </p>
@@ -57,11 +55,10 @@
           <p class="text-md text-gray-300">Trent University</p>
 
           <ul class="text-md  pt-2 list-disc flex flex-col text-gray-300">
-            <li>Experience using Laravel / Livewire to update existing applications.</li>
-            <li>Used Github PR's for feature focused development</li>
-            <li>PHPUnit for Unit and Feature testing</li>
-            <li>Created an API using Postman</li>
-            <li>Used Code with Me / Pair as needed</li>
+            <li>Created data driven reports in Request Tracker to support business decisions</li>
+            <li>Provided tech support for Students, Faculty, and Alumni</li>
+            <li>Developed user guides and help articles for an ITSM system</li>
+            <li>Researched ITSM systems and ITIL guidelines</li>
           </ul>
         </div>
       </div>
@@ -92,7 +89,7 @@
       <div
         class=" row-span-2 flex flex-col justify-center items-center border-1 border-white m-6 p-2 text-xl space-y-4">
         <h2 class="text-3xl text-center">Volunteer Experience</h2>
-        <div class="flex flex-col justify-around p-4">
+        <div class="flex flex-col justify-around p-2">
           <div class="flex flex-row justify-between space-x-16 items-end">
             <h3 class="text-2xl">Front-End Developer</h3>
             <p class="text-right text-md text-gray-300">October 2020 - January 2021</p>
@@ -101,7 +98,7 @@
         </div>
         <div class="flex flex-col justify-around p-2">
           <div class="flex flex-row justify-between space-x-16 items-end">
-            <h3 class="text-2xl">Chair / Member</h3>
+            <h3 class="text-2xl">Chair/Vice-Chair/Member</h3>
             <p class="text-right text-md text-gray-300">May 2014 - May 2018</p>
           </div>
           <p class="text-md text-gray-300">Peterborough Youth Council</p>
