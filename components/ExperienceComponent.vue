b2b888b6ce1b397f4a5c091585d685078e8d9566
Evan Campbell-Weiner <evancampbellweiner@gmail.com>
2022-02-20 15:29:30 -0500

    minor updates

---
+++ components/ExperienceComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="relative text-black bg-white p-4 rounded-md w-full">
+      <div class="flex flex-row items-center justify-between border-b border-gray-500">
+         <header class="flex flex-col items-start justify-center">
+         <h2 class="text-lg lg:text-md">{{info.jobtitle}}</h2>
+         <p class="text-gray-500  text-md lg:text-sm">{{info.company}}</p>
+         </header>
+         <p class="text-md lg:text-sm text-gray-500 justify-self-end self-end">{{info.dateStarted}} <span v-if="info.dateEnded">- {{info.dateEnded}}</span></p>
+      </div>
+      <ul class="list-disc pl-4">
+        <li class="text-md lg:text-sm" v-for="description in info.descriptions">{{description}}</li>
+      </ul>
+  </article>
+</template>
+<script>
+  export default {
+     props: {
+         info: {
+             jobtitle: String,
+            company: String,
+            descriptions: Array,
+            endDate: String,
+         }
+        
+
+      },
+     }
+</script>
