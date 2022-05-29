10d21084043afea232626bbeee1764ee847b1f01
Evan Campbell-Weiner <evancampbellweiner@gmail.com>
2022-05-29 15:07:48 -0400

    Some Professional Fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,18 +4,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'evancampbellweiner2022',
+    title: 'Evan Campbell-Weiner',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Evan Campbell-Weiner\'s Official Professional Website' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/EvanHill.jpg' }
     ]
   },
 
